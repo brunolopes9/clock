673ae9f742896641bfad0787c5bb22934815497d
Bruno Lopes <bruno-lopes9@hotmail.com>
2025-08-01 17:35:14 +0100

    Clock working

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 const POINTERHOUR = document.querySelector('#hour');
 const POINTERMINUTE = document.querySelector('#minute');
 const POINTERSECOND = document.querySelector('#second');
+
+function executarRelogio ()
+{
+
+
+var data = new Date();
+console.log(data);
+
+let hr=data.getHours();
+let min= data.getMinutes();
+let sec= data.getSeconds();
+console.log("Hora:" + hr + " Minuto: " + min + " Segundo: " + sec );
+
+let positionHr= (hr*360/12) + (min*(360/60)/12);
+let positionMin= (min*360/60) + (sec*(360/60)/60);
+let positionSec= sec * 360/60;
+
+
+POINTERHOUR.style.transform= "rotate(" + positionHr + "deg)";
+POINTERMINUTE.style.transform= "rotate(" + positionMin + "deg)";
+POINTERSECOND.style.transform= "rotate(" + positionSec + "deg)";
+}
+var interval = setInterval (executarRelogio,1000);
